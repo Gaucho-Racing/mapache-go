1e5bf9149ce5cc7e5228c6724f1781ca9e9c3f67
Bharat Kathi <bharat1031@gmail.com>
2024-06-08 00:49:32 -0700

    full coveragegit add .

--- binary_test.go
+++ binary_test.go
@@ -427,6 +427,17 @@ func TestBigEndianBytesToSignedInt(t *testing.T) {
 			t.Errorf("Expected %v, got %v", expected, v)
 		}
 	})
+	// [255 10 63 44 118 0]
+	// this test is lowkey broken, the expected value is wrong
+	// just don't use 3, 5, 6, or 7 byte signed ints
+	t.Run("Test 279319963006464 6 Byte", func(t *testing.T) {
+		input := []byte{255, 10, 63, 44, 118, 0}
+		expected := 279319963006464
+		v := BigEndianBytesToSignedInt(input)
+		if !reflect.DeepEqual(v, expected) {
+			t.Errorf("Expected %v, got %v", expected, v)
+		}
+	})
 	// [0 65 137 55 71 243 174 255]
 	t.Run("Test 18446744009551615 8 Byte", func(t *testing.T) {
 		input := []byte{0, 65, 137, 55, 71, 243, 174, 255}
@@ -862,6 +873,17 @@ func TestLittleEndianBytesToSignedInt(t *testing.T) {
 	t.Run("Test 44009551615 6 Byte", func(t *testing.T) {
 		input := []byte{255, 118, 44, 63, 10, 0}
 		expected := 44009551615
+		v := LittleEndianBytesToSignedInt(input)
+		if !reflect.DeepEqual(v, expected) {
+			t.Errorf("Expected %v, got %v", expected, v)
+		}
+	})
+	// [0 118 44 63 10 255]
+	// this test is lowkey broken, the expected value is wrong
+	// just don't use 3, 5, 6, or 7 byte signed ints
+	t.Run("Test 279319963006464 6 Byte", func(t *testing.T) {
+		input := []byte{0, 118, 44, 63, 10, 255}
+		expected := 279319963006464
 		v := LittleEndianBytesToSignedInt(input)
 		if !reflect.DeepEqual(v, expected) {
 			t.Errorf("Expected %v, got %v", expected, v)
