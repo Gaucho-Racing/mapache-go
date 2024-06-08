025490f4f015a84fb2f170de6ef4716457a7b5dc
Bharat Kathi <bharat1031@gmail.com>
2024-06-07 18:49:01 -0700

    add tests

--- binary_test.go
+++ binary_test.go
@@ -1,6 +1,11 @@
 package mapache
 
-import "testing"
+import (
+	"encoding/binary"
+	"fmt"
+	"reflect"
+	"testing"
+)
 
 func TestBigEndianUnsignedIntToBinaryString(t *testing.T) {
 	t.Run("Test Negative Number", func(t *testing.T) {
@@ -19,6 +24,13 @@ func TestBigEndianUnsignedIntToBinaryString(t *testing.T) {
 		_, err := BigEndianUnsignedIntToBinaryString(31241, 1)
 		if err == nil {
 			t.Error("Expected error, got nil")
+		}
+	})
+	t.Run("Test 38134 2 Byte", func(t *testing.T) {
+		v, _ := BigEndianUnsignedIntToBinaryString(38134, 2)
+		expected := "1001010011110110"
+		if v != expected {
+			t.Errorf("Expected %v, got %v", expected, v)
 		}
 	})
 }
@@ -40,6 +52,132 @@ func TestBigEndianUnsignedIntToBinary(t *testing.T) {
 		_, err := BigEndianUnsignedIntToBinary(31241, 1)
 		if err == nil {
 			t.Error("Expected error, got nil")
+		}
+	})
+	t.Run("Test Number Too Large 2", func(t *testing.T) {
+		_, err := BigEndianUnsignedIntToBinary(3172123, 2)
+		if err == nil {
+			t.Error("Expected error, got nil")
+		}
+	})
+	t.Run("Test 0 1 Byte", func(t *testing.T) {
+		v, _ := BigEndianUnsignedIntToBinary(0, 1)
+		expected := []byte{0}
+		if !reflect.DeepEqual(v, expected) {
+			t.Errorf("Expected %v, got %v", expected, v)
+		}
+	})
+	t.Run("Test 123 1 Byte", func(t *testing.T) {
+		v, _ := BigEndianUnsignedIntToBinary(123, 1)
+		expected := []byte{123}
+		if !reflect.DeepEqual(v, expected) {
+			t.Errorf("Expected %v, got %v", expected, v)
+		}
+	})
+	t.Run("Test 255 1 Byte", func(t *testing.T) {
+		v, _ := BigEndianUnsignedIntToBinary(255, 1)
+		expected := []byte{255}
+		if !reflect.DeepEqual(v, expected) {
+			t.Errorf("Expected %v, got %v", expected, v)
+		}
+	})
+	t.Run("Test 172 2 Byte", func(t *testing.T) {
+		v, _ := BigEndianUnsignedIntToBinary(172, 2)
+		expected := make([]byte, 2)
+		binary.BigEndian.PutUint16(expected, uint16(172))
+		fmt.Printf("%v\n", expected)
+		if !reflect.DeepEqual(v, expected) {
+			t.Errorf("Expected %v, got %v", expected, v)
+		}
+	})
+	t.Run("Test 38134 2 Byte", func(t *testing.T) {
+		v, _ := BigEndianUnsignedIntToBinary(38134, 2)
+		expected := make([]byte, 2)
+		binary.BigEndian.PutUint16(expected, uint16(38134))
+		fmt.Printf("%v\n", expected)
+		if !reflect.DeepEqual(v, expected) {
+			t.Errorf("Expected %v, got %v", expected, v)
+		}
+	})
+}
+
+func TestBigEndianSignedIntToBinaryString(t *testing.T) {
+	t.Run("Test 0 Bytes", func(t *testing.T) {
+		_, err := BigEndianSignedIntToBinaryString(100, 0)
+		if err == nil {
+			t.Error("Expected error, got nil")
+		}
+	})
+	t.Run("Test Number Too Large", func(t *testing.T) {
+		_, err := BigEndianSignedIntToBinaryString(31241, 1)
+		if err == nil {
+			t.Error("Expected error, got nil")
+		}
+	})
+	t.Run("Test 38134 2 Byte", func(t *testing.T) {
+		v, _ := BigEndianSignedIntToBinaryString(38134, 2)
+		expected := "1001010011110110"
+		if v != expected {
+			t.Errorf("Expected %v, got %v", expected, v)
+		}
+	})
+}
+
+func TestBigEndianSignedIntToBinary(t *testing.T) {
+	t.Run("Test 0 Bytes", func(t *testing.T) {
+		_, err := BigEndianSignedIntToBinary(100, 0)
+		if err == nil {
+			t.Error("Expected error, got nil")
+		}
+	})
+	t.Run("Test Number Too Large", func(t *testing.T) {
+		_, err := BigEndianSignedIntToBinary(31241, 1)
+		if err == nil {
+			t.Error("Expected error, got nil")
+		}
+	})
+	t.Run("Test Number Too Large 2", func(t *testing.T) {
+		_, err := BigEndianSignedIntToBinary(3172123, 2)
+		if err == nil {
+			t.Error("Expected error, got nil")
+		}
+	})
+	t.Run("Test 0 1 Byte", func(t *testing.T) {
+		v, _ := BigEndianSignedIntToBinary(0, 1)
+		expected := []byte{0}
+		if !reflect.DeepEqual(v, expected) {
+			t.Errorf("Expected %v, got %v", expected, v)
+		}
+	})
+	t.Run("Test 123 1 Byte", func(t *testing.T) {
+		v, _ := BigEndianSignedIntToBinary(123, 1)
+		expected := []byte{123}
+		if !reflect.DeepEqual(v, expected) {
+			t.Errorf("Expected %v, got %v", expected, v)
+		}
+	})
+	t.Run("Test 255 1 Byte", func(t *testing.T) {
+		_, err := BigEndianSignedIntToBinary(255, 1)
+		if err == nil {
+			t.Error("Expected error, got nil")
+		}
+	})
+	t.Run("Test 172 2 Byte", func(t *testing.T) {
+		v, _ := BigEndianSignedIntToBinary(172, 2)
+		expected := make([]byte, 2)
+		binary.BigEndian.PutUint16(expected, uint16(172))
+		fmt.Printf("%v\n", expected)
+		if !reflect.DeepEqual(v, expected) {
+			t.Errorf("Expected %v, got %v", expected, v)
+		}
+	})
+	t.Run("Test 38134 2 Byte", func(t *testing.T) {
+		v, _ := BigEndianSignedIntToBinary(38134, 2)
+		expected := make([]byte, 2)
+		binary.BigEndian.PutUint16(expected, uint16(38134))
+		fmt.Printf("%v\n", expected)
+		if !reflect.DeepEqual(v, expected) {
+			t.Errorf("Expected %v, got %v", expected, v)
 		}
 	})
 }
